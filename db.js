ad78cc6d025fa85a32ddf3747db56b2d74155b3b
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-05-10 09:59:20 +0600

    working with db.js file

--- db.js
+++ db.js
@@ -7,5 +7,12 @@ console.log(path.resolve(process.env.DB_URL));
 class DatabaseConnection {
   constructor() {
     this.db = null;
+    this.dbURL = path.resolve(process.env.DB_URL);
   }
+
+  async readDB() {
+    this.db = await fs.readFile(this.dbURL, { encoding: "utf-8" });
+  }
+
+  writeDB() {}
 }
